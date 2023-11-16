b949cb63654a08b804016428a3d9fc6b5c3173f3
DaoZhang <cx385918@digital-engine.com>
2023-11-16 16:17:54 +0800

    feat: 手动组装材质

--- react3d/src/Basic/16/index.tsx
+++ react3d/src/Basic/16/index.tsx
@@ -10,7 +10,8 @@ import * as TLightList from '../../assets/ts/TLight';
 import * as THelperList from '../../assets/ts/THelper';
 import * as TCodeModel from '../../assets/ts/TCodeModel';
 import './index.css';
-import { femalePromise } from '../../assets/ts/TLoaderModel';
+import { cerberusMaterial, cerberusPromise, femalePromise } from '../../assets/ts/TLoaderModel';
+import { Material, Mesh } from 'three';
 
 const App = () => {
 	const threeCanvas = useRef(null);
@@ -20,12 +21,28 @@ const App = () => {
 		TE.current = new TEngine(threeCanvas.current!);
 		TE.current.addObject(...Object.values(TLightList));
 		TE.current.addObject(...Object.values(THelperList));
-		TE.current.addObject(...Object.values(TBasicObjectList));
+		// TE.current.addObject(...Object.values(TBasicObjectList));
 		TE.current.addObject(...Object.values(TCodeModel));
 		femalePromise
 			.then((group) => {
 				group.scale.set(0.5, 0.5, 0.5);
 				group.position.z = 10;
+				// TE.current?.addObject(group)
+			})
+			.catch(e => console.log(e));
+
+		cerberusPromise
+			.then((group) => {
+				const cerberusMesh: Mesh = group.children[0] as Mesh;
+				// 先清空材质
+				(cerberusMesh.material as Material).dispose();
+				// 再设置材质
+				cerberusMesh.material = cerberusMaterial;
+
+				group.scale.set(20, 20, 20);
+				group.rotateY(Math.PI);
+				group.position.set(-10, 50, 0);
+				
 				TE.current?.addObject(group)
 			})
 			.catch(e => console.log(e));
--- react3d/src/assets/ts/TLoaderModel.ts
+++ react3d/src/assets/ts/TLoaderModel.ts
@@ -1,20 +1,50 @@
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
 import { getAssetUrl } from "../../Basic/utils";
+import { MeshStandardMaterial, RepeatWrapping, SRGBColorSpace, TextureLoader } from "three";
 
 const fileUrl = import.meta.url;
 const objLoader = new OBJLoader();
 const mtllLoader = new MTLLoader();
+const textureLoader = new TextureLoader();
 
-// 1、导入材质
+/* 
+  导入材质有两种方案：
+  1、导入组装好的材质文件 mtl
+  2、手动组装材质
+ */
+// 1、导入组装好的材质文件
 const mtll = getAssetUrl('./../obj/female02/female02.mtl', fileUrl)
-const materials = await mtllLoader.loadAsync(mtll);
-
+const femaleMaterials = await mtllLoader.loadAsync(mtll);
 // 导入模型
-const objModel = getAssetUrl('./../obj/female02/female02.obj', fileUrl);
+const femaleModel = getAssetUrl('./../obj/female02/female02.obj', fileUrl);
 // 加载带有材质的模型
 export const femalePromise = objLoader
-  .setMaterials(materials)
-  .loadAsync(objModel);
+  .setMaterials(femaleMaterials)
+  .loadAsync(femaleModel);
 // 导入材质
 
+// 2、手动组装材质贴图
+// 颜色贴图
+const C_Url = getAssetUrl('./../obj/cerberus/Cerberus_A.jpg', fileUrl)
+const colorTexture = textureLoader.load(C_Url);
+// 法线贴图的纹理
+const R_Url = getAssetUrl('./../obj/cerberus/Cerberus_N.jpg', fileUrl)
+const normalTexture = textureLoader.load(R_Url);
+// 位移纹理贴图
+const D_Url = getAssetUrl('./../obj/cerberus/Cerberus_RM.jpg', fileUrl)
+const metalnessTexture = textureLoader.load(D_Url);
+// 凹凸纹理贴图
+const B_Url = getAssetUrl('./../obj/cerberus/Cerberus_M.jpg', fileUrl)
+const bumpTexture = textureLoader.load(B_Url);
+// 合并材质
+export const cerberusMaterial = new MeshStandardMaterial({
+  map: colorTexture,
+  metalness: 1,
+  metalnessMap: metalnessTexture,
+  normalMap: normalTexture,
+  bumpMap:bumpTexture
+})
+// 导入模型
+const cerberusModel = getAssetUrl('./../obj/cerberus/Cerberus.obj', fileUrl);
+export const cerberusPromise = objLoader.loadAsync(cerberusModel);
