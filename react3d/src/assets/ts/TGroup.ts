262d36442bbc5ca4d286a3b665c941ab31e51c0e
DaoZhang <cx385918@digital-engine.com>
2023-12-08 15:28:47 +0800

    feat: 使用 sprite 绘制 tips

--- react3d/src/assets/ts/TGroup.ts
+++ react3d/src/assets/ts/TGroup.ts
@@ -1,4 +1,12 @@
-import { BoxGeometry, Group, Mesh, MeshBasicMaterial, PlaneGeometry } from "three";
+/* 
+精灵
+传统意义：Sprite是性能不足下的一种硬件图形加速手段，用于显示自由活动的二维图形块。
+将需要重绘的部分单独设置为一个图层，减少重绘压力
+
+现代意义：Sprite是一种软件层面的抽象概念，用于表示并处理自由活动的对象。仅使用CPU进行2D图像渲染已非常常见。但为了减少不必要的重绘（比如两帧中不变的部分），开发者仍然可以采取类似的处理方式，将自行定义的Sprite绘制在特定的缓冲层，在显示的时候再将各层叠加得到最终画面。而这些Sprite，更多是为了简化开发流程而人为规定的模型和概念，与硬件不再是强相关了。
+ */
+
+import { BoxGeometry, Group, Mesh, MeshBasicMaterial, PlaneGeometry, Sprite, SpriteMaterial } from "three";
 import { pictureTexture, tipsTexture } from './TTextures';
 
 export const groupPromise = new Promise<Group>((resolve) => {
@@ -16,16 +24,26 @@ export const groupPromise = new Promise<Group>((resolve) => {
   picture.scale.set(0.3, 0.3, 0.3);
   
   // 标签
-  const tips = new Mesh(
-    new PlaneGeometry(19, 10),
-    new MeshBasicMaterial({
-      map: tipsTexture,
-    })
-  )
+  // const tips = new Mesh(
+  //   new PlaneGeometry(19, 10),
+  //   new MeshBasicMaterial({
+  //     map: tipsTexture,
+  //   })
+  // )
+  // tips.position.set(0, 60, 0);
+  // tips.onBeforeRender = (renderer, screen, camera) => {
+  //   tips.lookAt(camera.position);
+  // }
+
+  // sprite: 不于画布融为一体，像一个幽灵悬浮于画面之外。
+  const tips: Sprite = new Sprite(new SpriteMaterial({
+    map: tipsTexture,
+    sizeAttenuation: false, // 无尺寸衰减
+    depthTest: false, // 不进行深度确认
+    depthWrite: false,  // 不影响深度缓冲区域
+  }));
   tips.position.set(0, 60, 0);
-  tips.onBeforeRender = (renderer, screen, camera) => {
-    tips.lookAt(camera.position);
-  }
+  tips.scale.set(0.16,0.09,1);
 
   // 相框 
   const plan = new Mesh(
